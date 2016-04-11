35906e595e3a7c6f7cc4917aa3e4dc12b8df9b92
RUIKK069 <ruikawano069@gmail.com>
2016-04-10 22:58:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,45 @@
 #include<stdio.h>
-double raizq(double);
 int main(void)
 {
-double n,X,Y;
-scanf("%1f",&n);
-X=n/X;
-Y=X;
-printf("%2f\n",Y);
-}
-double raizq(double Y)
-{
-return 0.0;
-}
+int Y,Z,n,raizint,AUX;
+double X,raiz,raizdec;
+Y=2;
+raiz=0;
+raizint=0;
+raizdec=0;
+n=0;
+scanf("%d",&X);
+while (n<=X) {
+  if (X>n*n){
+  n=n+1;
+  }else{
+  if (X=n*n) {
+  raiz=n;
+  n=X+1;
+  }else{
+  raizint=n-1;
+  n=X+1;
+  }}}
+if ((raiz=0)&&(X!=0)){
+  if (Y=0){
+  raiz=raizint;
+  }else{
+  AUX=1;
+  Z=1;
+  raizdec=raizint;
+  while (AUX<=Y){
+  raizdec=raizdec+(1/(10*Z));
+  if (X>raizdec*raizdec){
+  }else{
+  if (X=raizdec*raizdec){
+  AUX=99;
+  }else{
+  raizdec=raizdec-(1/(10*Z));
+  AUX=AUX+1;
+  Z=Z*10;
+  }}}}
+  raiz=raizdec;
+  }
+printf("A raiz quadrada de %d é %f",X,raiz)
+return 0;
+  }
